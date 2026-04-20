e45d237413c110b127ffdfc8f5109c7b8003442d
YO!CHI KIKUCHI <i@yo1000.com>
2026-04-20 11:04:20 +0900

    Fix resource closing in tests

--- src/test/java/com/yo1000/postcode/infrastructure/ResourceZippedCsvFileLoaderTests.java
+++ src/test/java/com/yo1000/postcode/infrastructure/ResourceZippedCsvFileLoaderTests.java
@@ -49,61 +49,61 @@ public class ResourceZippedCsvFileLoaderTests {
         ResourceZippedCsvFileLoader<PostCsv, PostCsv> loader = new ResourceZippedCsvFileLoader<>(appProps);
 
         // When
-        CloseableIterator<PostCsv> iter = loader.load(row -> row);
+        try (CloseableIterator<PostCsv> iter = loader.load(row -> row)) {
+            // Then
+            List<PostCsv> posts = StreamSupport.stream(Spliterators.spliteratorUnknownSize(
+                    iter, Spliterator.ORDERED), false).toList();
 
-        // Then
-        List<PostCsv> posts = StreamSupport.stream(Spliterators.spliteratorUnknownSize(
-                iter, Spliterator.ORDERED), false).toList();
+            Assertions.assertThat(posts.size()).isEqualTo(3);
 
-        Assertions.assertThat(posts.size()).isEqualTo(3);
+            Assertions.assertThat(posts.get(0).localGovCode()).isEqualTo("01101");
+            Assertions.assertThat(posts.get(0).postcode5()).isEqualTo("060  ");
+            Assertions.assertThat(posts.get(0).postcode7()).isEqualTo("0600000");
+            Assertions.assertThat(posts.get(0).prefectureName()).isEqualTo("北海道");
+            Assertions.assertThat(posts.get(0).municipalityName()).isEqualTo("札幌市中央区");
+            Assertions.assertThat(posts.get(0).townAreaName()).isEqualTo("以下に掲載がない場合");
+            Assertions.assertThat(posts.get(0).prefectureNameKatakana()).isEqualTo("ホッカイドウ");
+            Assertions.assertThat(posts.get(0).municipalityNameKatakana()).isEqualTo("サッポロシチュウオウク");
+            Assertions.assertThat(posts.get(0).townAreaNameKatakana()).isEqualTo("イカニケイサイガナイバアイ");
+            Assertions.assertThat(posts.get(0).townAreaWithMultiplePostcodes()).isEqualTo("0");
+            Assertions.assertThat(posts.get(0).townAreaWithAddressNumbersPerKoaza()).isEqualTo("0");
+            Assertions.assertThat(posts.get(0).townAreaWithChome()).isEqualTo("0");
+            Assertions.assertThat(posts.get(0).postcodeWithMultipleTownAreas()).isEqualTo("0");
+            Assertions.assertThat(posts.get(0).changed()).isEqualTo("0");
+            Assertions.assertThat(posts.get(0).changeReason()).isEqualTo("0");
 
-        Assertions.assertThat(posts.get(0).localGovCode()).isEqualTo("01101");
-        Assertions.assertThat(posts.get(0).postcode5()).isEqualTo("060  ");
-        Assertions.assertThat(posts.get(0).postcode7()).isEqualTo("0600000");
-        Assertions.assertThat(posts.get(0).prefectureName()).isEqualTo("北海道");
-        Assertions.assertThat(posts.get(0).municipalityName()).isEqualTo("札幌市中央区");
-        Assertions.assertThat(posts.get(0).townAreaName()).isEqualTo("以下に掲載がない場合");
-        Assertions.assertThat(posts.get(0).prefectureNameKatakana()).isEqualTo("ホッカイドウ");
-        Assertions.assertThat(posts.get(0).municipalityNameKatakana()).isEqualTo("サッポロシチュウオウク");
-        Assertions.assertThat(posts.get(0).townAreaNameKatakana()).isEqualTo("イカニケイサイガナイバアイ");
-        Assertions.assertThat(posts.get(0).townAreaWithMultiplePostcodes()).isEqualTo("0");
-        Assertions.assertThat(posts.get(0).townAreaWithAddressNumbersPerKoaza()).isEqualTo("0");
-        Assertions.assertThat(posts.get(0).townAreaWithChome()).isEqualTo("0");
-        Assertions.assertThat(posts.get(0).postcodeWithMultipleTownAreas()).isEqualTo("0");
-        Assertions.assertThat(posts.get(0).changed()).isEqualTo("0");
-        Assertions.assertThat(posts.get(0).changeReason()).isEqualTo("0");
+            Assertions.assertThat(posts.get(1).localGovCode()).isEqualTo("13101");
+            Assertions.assertThat(posts.get(1).postcode5()).isEqualTo("100  ");
+            Assertions.assertThat(posts.get(1).postcode7()).isEqualTo("1000000");
+            Assertions.assertThat(posts.get(1).prefectureName()).isEqualTo("東京都");
+            Assertions.assertThat(posts.get(1).municipalityName()).isEqualTo("千代田区");
+            Assertions.assertThat(posts.get(1).townAreaName()).isEqualTo("以下に掲載がない場合");
+            Assertions.assertThat(posts.get(1).prefectureNameKatakana()).isEqualTo("トウキョウト");
+            Assertions.assertThat(posts.get(1).municipalityNameKatakana()).isEqualTo("チヨダク");
+            Assertions.assertThat(posts.get(1).townAreaNameKatakana()).isEqualTo("イカニケイサイガナイバアイ");
+            Assertions.assertThat(posts.get(1).townAreaWithMultiplePostcodes()).isEqualTo("0");
+            Assertions.assertThat(posts.get(1).townAreaWithAddressNumbersPerKoaza()).isEqualTo("0");
+            Assertions.assertThat(posts.get(1).townAreaWithChome()).isEqualTo("0");
+            Assertions.assertThat(posts.get(1).postcodeWithMultipleTownAreas()).isEqualTo("0");
+            Assertions.assertThat(posts.get(1).changed()).isEqualTo("0");
+            Assertions.assertThat(posts.get(1).changeReason()).isEqualTo("0");
 
-        Assertions.assertThat(posts.get(1).localGovCode()).isEqualTo("13101");
-        Assertions.assertThat(posts.get(1).postcode5()).isEqualTo("100  ");
-        Assertions.assertThat(posts.get(1).postcode7()).isEqualTo("1000000");
-        Assertions.assertThat(posts.get(1).prefectureName()).isEqualTo("東京都");
-        Assertions.assertThat(posts.get(1).municipalityName()).isEqualTo("千代田区");
-        Assertions.assertThat(posts.get(1).townAreaName()).isEqualTo("以下に掲載がない場合");
-        Assertions.assertThat(posts.get(1).prefectureNameKatakana()).isEqualTo("トウキョウト");
-        Assertions.assertThat(posts.get(1).municipalityNameKatakana()).isEqualTo("チヨダク");
-        Assertions.assertThat(posts.get(1).townAreaNameKatakana()).isEqualTo("イカニケイサイガナイバアイ");
-        Assertions.assertThat(posts.get(1).townAreaWithMultiplePostcodes()).isEqualTo("0");
-        Assertions.assertThat(posts.get(1).townAreaWithAddressNumbersPerKoaza()).isEqualTo("0");
-        Assertions.assertThat(posts.get(1).townAreaWithChome()).isEqualTo("0");
-        Assertions.assertThat(posts.get(1).postcodeWithMultipleTownAreas()).isEqualTo("0");
-        Assertions.assertThat(posts.get(1).changed()).isEqualTo("0");
-        Assertions.assertThat(posts.get(1).changeReason()).isEqualTo("0");
-
-        Assertions.assertThat(posts.get(2).localGovCode()).isEqualTo("14101");
-        Assertions.assertThat(posts.get(2).postcode5()).isEqualTo("230  ");
-        Assertions.assertThat(posts.get(2).postcode7()).isEqualTo("2300000");
-        Assertions.assertThat(posts.get(2).prefectureName()).isEqualTo("神奈川県");
-        Assertions.assertThat(posts.get(2).municipalityName()).isEqualTo("横浜市鶴見区");
-        Assertions.assertThat(posts.get(2).townAreaName()).isEqualTo("以下に掲載がない場合");
-        Assertions.assertThat(posts.get(2).prefectureNameKatakana()).isEqualTo("カナガワケン");
-        Assertions.assertThat(posts.get(2).municipalityNameKatakana()).isEqualTo("ヨコハマシツルミク");
-        Assertions.assertThat(posts.get(2).townAreaNameKatakana()).isEqualTo("イカニケイサイガナイバアイ");
-        Assertions.assertThat(posts.get(2).townAreaWithMultiplePostcodes()).isEqualTo("0");
-        Assertions.assertThat(posts.get(2).townAreaWithAddressNumbersPerKoaza()).isEqualTo("0");
-        Assertions.assertThat(posts.get(2).townAreaWithChome()).isEqualTo("0");
-        Assertions.assertThat(posts.get(2).postcodeWithMultipleTownAreas()).isEqualTo("0");
-        Assertions.assertThat(posts.get(2).changed()).isEqualTo("0");
-        Assertions.assertThat(posts.get(2).changeReason()).isEqualTo("0");
+            Assertions.assertThat(posts.get(2).localGovCode()).isEqualTo("14101");
+            Assertions.assertThat(posts.get(2).postcode5()).isEqualTo("230  ");
+            Assertions.assertThat(posts.get(2).postcode7()).isEqualTo("2300000");
+            Assertions.assertThat(posts.get(2).prefectureName()).isEqualTo("神奈川県");
+            Assertions.assertThat(posts.get(2).municipalityName()).isEqualTo("横浜市鶴見区");
+            Assertions.assertThat(posts.get(2).townAreaName()).isEqualTo("以下に掲載がない場合");
+            Assertions.assertThat(posts.get(2).prefectureNameKatakana()).isEqualTo("カナガワケン");
+            Assertions.assertThat(posts.get(2).municipalityNameKatakana()).isEqualTo("ヨコハマシツルミク");
+            Assertions.assertThat(posts.get(2).townAreaNameKatakana()).isEqualTo("イカニケイサイガナイバアイ");
+            Assertions.assertThat(posts.get(2).townAreaWithMultiplePostcodes()).isEqualTo("0");
+            Assertions.assertThat(posts.get(2).townAreaWithAddressNumbersPerKoaza()).isEqualTo("0");
+            Assertions.assertThat(posts.get(2).townAreaWithChome()).isEqualTo("0");
+            Assertions.assertThat(posts.get(2).postcodeWithMultipleTownAreas()).isEqualTo("0");
+            Assertions.assertThat(posts.get(2).changed()).isEqualTo("0");
+            Assertions.assertThat(posts.get(2).changeReason()).isEqualTo("0");
+        }
     }
 
     @Test
@@ -133,28 +133,28 @@ public class ResourceZippedCsvFileLoaderTests {
         ResourceZippedCsvFileLoader<PostCsv, PostCsv> loader = new ResourceZippedCsvFileLoader<>(appProps);
 
         // When
-        CloseableIterator<PostCsv> iter = loader.load(row -> row);
+        try (CloseableIterator<PostCsv> iter = loader.load(row -> row)) {
+            // Then
+            List<PostCsv> posts = StreamSupport.stream(Spliterators.spliteratorUnknownSize(
+                    iter, Spliterator.ORDERED), false).toList();
 
-        // Then
-        List<PostCsv> posts = StreamSupport.stream(Spliterators.spliteratorUnknownSize(
-                iter, Spliterator.ORDERED), false).toList();
+            Assertions.assertThat(posts.size()).isEqualTo(1);
 
-        Assertions.assertThat(posts.size()).isEqualTo(1);
-
-        Assertions.assertThat(posts.get(0).localGovCode()).isEqualTo("");
-        Assertions.assertThat(posts.get(0).postcode5()).isEqualTo("");
-        Assertions.assertThat(posts.get(0).postcode7()).isEqualTo("");
-        Assertions.assertThat(posts.get(0).prefectureName()).isEqualTo("");
-        Assertions.assertThat(posts.get(0).municipalityName()).isEqualTo("");
-        Assertions.assertThat(posts.get(0).townAreaName()).isEqualTo("");
-        Assertions.assertThat(posts.get(0).prefectureNameKatakana()).isEqualTo("");
-        Assertions.assertThat(posts.get(0).municipalityNameKatakana()).isEqualTo("");
-        Assertions.assertThat(posts.get(0).townAreaNameKatakana()).isEqualTo("");
-        Assertions.assertThat(posts.get(0).townAreaWithMultiplePostcodes()).isEqualTo("");
-        Assertions.assertThat(posts.get(0).townAreaWithAddressNumbersPerKoaza()).isEqualTo("");
-        Assertions.assertThat(posts.get(0).townAreaWithChome()).isEqualTo("");
-        Assertions.assertThat(posts.get(0).postcodeWithMultipleTownAreas()).isEqualTo("");
-        Assertions.assertThat(posts.get(0).changed()).isEqualTo("");
-        Assertions.assertThat(posts.get(0).changeReason()).isEqualTo("");
+            Assertions.assertThat(posts.get(0).localGovCode()).isEqualTo("");
+            Assertions.assertThat(posts.get(0).postcode5()).isEqualTo("");
+            Assertions.assertThat(posts.get(0).postcode7()).isEqualTo("");
+            Assertions.assertThat(posts.get(0).prefectureName()).isEqualTo("");
+            Assertions.assertThat(posts.get(0).municipalityName()).isEqualTo("");
+            Assertions.assertThat(posts.get(0).townAreaName()).isEqualTo("");
+            Assertions.assertThat(posts.get(0).prefectureNameKatakana()).isEqualTo("");
+            Assertions.assertThat(posts.get(0).municipalityNameKatakana()).isEqualTo("");
+            Assertions.assertThat(posts.get(0).townAreaNameKatakana()).isEqualTo("");
+            Assertions.assertThat(posts.get(0).townAreaWithMultiplePostcodes()).isEqualTo("");
+            Assertions.assertThat(posts.get(0).townAreaWithAddressNumbersPerKoaza()).isEqualTo("");
+            Assertions.assertThat(posts.get(0).townAreaWithChome()).isEqualTo("");
+            Assertions.assertThat(posts.get(0).postcodeWithMultipleTownAreas()).isEqualTo("");
+            Assertions.assertThat(posts.get(0).changed()).isEqualTo("");
+            Assertions.assertThat(posts.get(0).changeReason()).isEqualTo("");
+        }
     }
 }
