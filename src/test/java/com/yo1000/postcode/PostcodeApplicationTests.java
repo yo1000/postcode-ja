962b864316b4fc2eaaf9e1c9284d22b4b8a2baa0
YO!CHI KIKUCHI <i@yo1000.com>
2026-04-20 11:54:57 +0900

    Remove useless imports

--- src/test/java/com/yo1000/postcode/PostcodeApplicationTests.java
+++ src/test/java/com/yo1000/postcode/PostcodeApplicationTests.java
@@ -2,7 +2,6 @@ package com.yo1000.postcode;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.junit.jupiter.Container;
